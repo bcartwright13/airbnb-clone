c791890b112d4081264110b81a814dd469dac9d7
Brandon Cartwright <bcartwright16@gmail.com>
2023-10-25 21:32:23 -0600

    Start Card.js

---
+++ src/components/Card.js
@@ -0,0 +1,19 @@
+import React from "react"
+import image from "../images/katiee-zaferes.png"
+import star from "../images/Red_star.svg.png"
+export default function Card(){
+    return (
+        <div>
+            <div class = "card">
+                <img src = {image} alt="katie zaferes" width="200px"/>
+                <div class="below-image">
+                    <img class="star" src ={star} alt="red star"/>
+                    <p class="rating">5.0</p>
+                    <p class="trend-number">(5) USA</p>
+                </div>
+                <p>Life lessons with Katie Zaferes</p>
+                <p><strong>From $136</strong> / person</p>
+            </div>
+        </div>
+    )
+}
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,7 +3,7 @@ import image from "../images/hero-image.png"
 export default function Hero(){
     return (
         <div class ="hero">
-            <img class="hero-image"src={image} alt="image of people" width="400px"/>
+            <img class="hero-image"src={image} alt="people" width="400px"/>
             <h1>Online Experiences</h1>
             <p>Join unique interactive activities led by<br/>
                 one-of-a-kind hosts-all without leaving<br/>

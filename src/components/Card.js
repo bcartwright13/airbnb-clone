2c957d7551aa4dc4d8e35c77014f27bcdb71db95
Brandon Cartwright <bcartwright16@gmail.com>
2023-11-25 17:43:30 -0700

    Create cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,19 +3,16 @@ import star from "../images/Red_star.svg.png"
 
 export default function Card(props){
     return (
-        <div>
-            <div class = "card">
-                <img src= {`/images/${props.img}`} alt="Event image" width={200}/>
+        <div class = "card">
+            <img src= {`/images/${props.img}`} alt="Event image" width={200} height={280}/>
 
-                <div class="below-image">
-                    <img class="star" src ={star} alt="red star"/>
-                    <p class="rating">{props.rating}</p>
-                    <p class="trend-number">({props.reviewCount}) | {props.location}</p>
-                </div>
-                <p>{props.title}</p>
-                <p class = "price"><strong>From ${props.price}</strong> / person</p>
+            <div class="below-image">
+                <img class="star" src ={star} alt="red star"/>
+                <p class="rating">{props.rating}</p>
+                <p class="trend-number">({props.reviewCount}) | {props.location}</p>
             </div>
+            <p>{props.title}</p>
+            <p class = "price"><strong>From ${props.price}</strong> / person</p>
         </div>
     )
-
 }

6e9be46160a99b84e2ad55d145a11be568061188
Brandon Cartwright <bcartwright16@gmail.com>
2023-11-30 18:59:47 -0700

    Final Version

--- src/App.js
+++ src/App.js
@@ -10,12 +10,8 @@ function App() {
   const cards = cardData.map(card => {
     return(
       <Card 
-        img = {card.coverImg}
-        rating = {card.stats.rating}
-        reviewCount = {card.stats.reviewCount}
-        location = {card.location}
-        title = {card.title}
-        price = {card.price}
+        key={card.id}
+        {...card}
       />
     )
   })
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,14 +2,21 @@ import React from "react"
 import star from "../images/Red_star.svg.png"
 
 export default function Card(props){
+    let badgeText
+    if(props.openSpots === 0){
+        badgeText = "SOLD OUT"
+    }else if(props.location === "Online"){
+        badgeText = "ONLINE"
+    }
     return (
         <div class = "card">
-            <img src= {`/images/${props.img}`} alt="Event image" width={200} height={280}/>
+            {badgeText != null && <div className="card-badge">{badgeText}</div>}
+            <img src= {`/images/${props.coverImg}`} alt="Event" width={200} height={260}/>
 
             <div class="below-image">
                 <img class="star" src ={star} alt="red star"/>
-                <p class="rating">{props.rating}</p>
-                <p class="trend-number">({props.reviewCount}) | {props.location}</p>
+                <p class="rating">{props.stats.rating}</p>
+                <p class="trend-number">({props.stats.reviewCount}) | {props.location}</p>
             </div>
             <p>{props.title}</p>
             <p class = "price"><strong>From ${props.price}</strong> / person</p>
